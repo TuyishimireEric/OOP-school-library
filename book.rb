4bf8f14a3173fb9a2be6cde831d05fa0449a233a
Tuyishimire Eric~ <tuyishimireericc@gmail.com>
2022-10-06 22:40:52 +0200

    fix linter errors

--- book.rb
+++ book.rb
@@ -1,13 +1,12 @@
 class Book
-    def initialize(title, author)
-      @title = title
-      @author = author
-    end
-  
-    def add_rental(date, person)
-      @rentals = Rental.new(date, self, person)
-    end
-  
-    attr_accessor :title, :author, :rentals
+  def initialize(title, author)
+    @title = title
+    @author = author
   end
-  
+
+  def add_rental(date, person)
+    @rentals = Rental.new(date, self, person)
+  end
+
+  attr_accessor :title, :author, :rentals
+end
