ba0724ac9a6920a8d659723b536d0218e53cf075
Tuyishimire Eric~ <tuyishimireericc@gmail.com>
2022-10-06 22:38:37 +0200

    made required changes

--- book.rb
+++ book.rb
@@ -1,9 +1,13 @@
 class Book
-  def initialize(title, author)
-    @title = title
-    @author = author
-    @rentals = []
+    def initialize(title, author)
+      @title = title
+      @author = author
+    end
+  
+    def add_rental(date, person)
+      @rentals = Rental.new(date, self, person)
+    end
+  
+    attr_accessor :title, :author, :rentals
   end
-
-  attr_accessor :title, :author, :rentals
-end
+  
